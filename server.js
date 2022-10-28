2aea2686b20d285f557b77f7ea58239712e9b7c4
Kevin Xu <kevinpxu21@gmail.com>
2022-10-27 21:14:05 -0500

    added post request to server.js

--- server.js
+++ server.js
@@ -24,7 +24,47 @@ app.get("/api/notes", (req, res) => {
   res.json(noteStorage);
 });
 
-//app.post("/api/notes", (req, res));
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received`);
+
+  const { title, text } = note;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      //TODO: may want to add unique id to note
+    };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const fileData = JSON.parse(data);
+        fileData.push(newNote);
+        noteStorage = fileData;
+        fs.writeFile(
+          "./db/reviews.json",
+          JSON.stringify(fileData, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated notes!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json("Error in posting notes");
+  }
+});
 
 //app.delete("/api/notes", (req, res));
 
